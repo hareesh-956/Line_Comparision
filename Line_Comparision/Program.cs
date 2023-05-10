fb35787401d4019e3c8698b27a87a04e0cb36be0
hareesh <hareesh.vanapalli.888@gmail.com>
2023-05-10 17:07:10 +0530

    [Hareesh]

--- Line_Comparision/Program.cs
+++ Line_Comparision/Program.cs
@@ -3,8 +3,34 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { 
             Console.WriteLine("Welcome Line Comparsion");
+            float x1;
+            float x2;
+            float y1;
+            float y2;
+            float length;
+            Console.WriteLine("please enter x1 value");
+            float.TryParse(Console.ReadLine(), out x1);
+
+            Console.WriteLine("please enter x2 value");
+            float.TryParse(Console.ReadLine(), out x2);
+
+            Console.WriteLine("please enter y1 value");
+            float.TryParse(Console.ReadLine(), out y1);
+
+            Console.WriteLine("please enter y2 value");
+            float.TryParse(Console.ReadLine(), out y2);
+
+            length = (float)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Length of Line" + length);
+
+
+
+
+
+
+
         }
     }
 }
