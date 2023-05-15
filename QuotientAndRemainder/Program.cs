f1cabf7363b3b3865b553f72b78caa1267fd910d
hareesh <hareesh.vanapalli.888@gmail.com>
2023-05-15 22:51:54 +0530

    [Hareesh]

---
+++ QuotientAndRemainder/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace QuotientAndRemainder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a divident: ");
+            int divident = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a divisor: ");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = divident / divisor;
+            int remainder = divident % divisor;
+
+            Console.WriteLine($"Quotient: {quotient}");
+            Console.WriteLine($"Remainder: {remainder}");
+
+
+        }
+    }
+}
